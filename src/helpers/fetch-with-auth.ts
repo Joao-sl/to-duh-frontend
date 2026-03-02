534d5d55cd275120d1b12cbb2c0184942fa7ec3e
João <joaosl.dev@gmail.com>
2026-03-02 18:19:48 -0300

    feat: add fetchWithAuth helper for authenticated request

---
+++ src/helpers/fetch-with-auth.ts
@@ -0,0 +1,38 @@
+import { getAccessToken, isTokenExpired } from './auth-token-manager';
+import { refreshAccessTokenAction } from '@/app/actions/auth/refresh-access-token';
+
+export async function fetchWithAuth(
+  input: string,
+  initOptions: RequestInit = {},
+) {
+  let access = await getAccessToken();
+
+  const makeRequest = (access: string | undefined) => {
+    return fetch(input, {
+      ...initOptions,
+      headers: { Authorization: `Bearer ${access}`, ...initOptions.headers },
+    });
+  };
+
+  const updateAccessToken = async () => {
+    await refreshAccessTokenAction();
+    access = await getAccessToken();
+  };
+
+  try {
+    if (!access || isTokenExpired(access)) {
+      await updateAccessToken();
+    }
+
+    const response = await makeRequest(access);
+
+    if (response.status === 401) {
+      await updateAccessToken();
+      await makeRequest(access);
+    }
+
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
