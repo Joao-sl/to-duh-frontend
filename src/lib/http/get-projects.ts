c0e2e1a5c7ba474af9cad7b56b44ad540fbb1bc7
João <joaosl.dev@gmail.com>
2026-03-12 17:40:55 -0300

    feat: add getProjects function to fetch user projects

---
+++ src/lib/http/get-projects.ts
@@ -0,0 +1,30 @@
+import { fetchWithAuth } from '@/helpers/fetch-with-auth';
+
+export type ProjectData = {
+  id: number;
+  user_id: number;
+  name: string;
+  description: string;
+  is_favorite: boolean;
+  is_archived: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+export async function getProjects() {
+  try {
+    const response = await fetchWithAuth(`${process.env.API_DOMAIN}/projects`, {
+      method: 'GET',
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return { success: false, ...data };
+    }
+
+    return { success: true, data: data };
+  } catch (error) {
+    return { success: false, error: error };
+  }
+}
