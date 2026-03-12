33f533d3d12ce630459fbdf70d55c2d627b45a1c
João <joaosl.dev@gmail.com>
2026-03-11 21:21:11 -0300

    feat: add slugify helper

---
+++ src/helpers/slugify.ts
@@ -0,0 +1,11 @@
+export function slugify(text: string) {
+  return text
+    .normalize('NFKD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, '')
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/^-|-$/g, '');
+}
