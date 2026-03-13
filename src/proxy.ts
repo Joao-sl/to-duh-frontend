0b988c54fe55c422e5583b0bb437416029a372a7
João <joaosl.dev@gmail.com>
2026-03-13 16:23:12 -0300

    feat: implement token revalidation and update proxy matcher
    - Implement accessToken revalidation when expired.
    - Add /account/:path* and /desk/:path* urls to proxy matcher.
    - Remove /hub/:path* url from matcher.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { REFRESH_COOKIE_NAME } from './helpers/auth-token-manager';
+import {
+  isTokenExpired,
+  ACCESS_COOKIE_NAME,
+  REFRESH_COOKIE_NAME,
+} from './helpers/auth-token-manager';
+import { refreshAccessTokenAction } from './app/actions/auth/refresh-access-token';
 
 export async function proxy(request: NextRequest) {
   const url = request.nextUrl.clone();
   const path = request.nextUrl.pathname;
   const isAuthPage = path === '/login' || path === '/signup';
   const isAuthenticated = request.cookies.get(REFRESH_COOKIE_NAME)?.value;
+  const accessToken = request.cookies.get(ACCESS_COOKIE_NAME)?.value;
 
   if (path.startsWith('/_next') || path.startsWith('/_static')) {
     return NextResponse.next();
@@ -18,7 +24,7 @@ export async function proxy(request: NextRequest) {
     (secFetchMode === 'navigate' || accept.includes('text/html'));
 
   if (isAuthenticated && isAuthPage && isNavigationRequest) {
-    url.pathname = '/hub';
+    url.pathname = '/desk';
     return NextResponse.redirect(url);
   }
 
@@ -27,9 +33,24 @@ export async function proxy(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (!accessToken || isTokenExpired(accessToken)) {
+    try {
+      await refreshAccessTokenAction();
+    } catch (error) {
+      if (process.env.NODE_ENV === 'development') {
+        console.log('PROXY ERROR: ', error);
+      }
+    }
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/login/:path*', '/signup/:path*', '/hub/:path*'],
+  matcher: [
+    '/login/:path*',
+    '/signup/:path*',
+    `/desk/:path*`,
+    '/account/:path*',
+  ],
 };
