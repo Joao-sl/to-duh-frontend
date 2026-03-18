ef826de4f309fbc4c12b9a043d3ab5ba09490edc
João <joaosl.dev@gmail.com>
2026-03-18 12:25:36 -0300

    feat: add internal AppSidebarHeader component
    - Render user avatar, name, and email
    - Navigate to /account and open edit modal on click

---
+++ src/components/app-sidebar/layout/header.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import Link from 'next/link';
+import { useUserContext } from '@/contexts/user-context';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+function AppSidebarHeader() {
+  const { user } = useUserContext();
+
+  return (
+    <SidebarHeader className='py-2'>
+      <SidebarGroupLabel>
+        <h2 className='flex items-center gap-1'>TO~DUH</h2>
+      </SidebarGroupLabel>
+
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            className='group-data-[collapsible=icon]:p-0! py-6 rounded-xl'
+            asChild
+          >
+            <Link
+              href='/account'
+              aria-label='Edit account details'
+              className='space-x-0.5'
+            >
+              <Avatar
+                aria-hidden
+                className='group-data-[collapsible=icon]:size-8! size-10'
+              >
+                <AvatarImage src={user?.avatar} />
+                <AvatarFallback>
+                  <div aria-label='Placeholder to your avatar image'>
+                    <span className='text-lg'>{user?.name[0] ?? 'T'}</span>
+                  </div>
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <p className='line-clamp-1'>{user?.name ?? 'ERROR'}</p>
+                <p className='line-clamp-1 text-muted-foreground text-xs'>
+                  {user?.email ?? 'ERROR'}
+                </p>
+              </div>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  );
+}
+
+export { AppSidebarHeader };
