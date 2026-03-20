eb7f5bfd0cf08c758e2102b62743ca93899a9673
João <joaosl.dev@gmail.com>
2026-03-20 13:43:16 -0300

    fix: access token refresh and simplify proxy auth flow
    - Update access token and insert to current request and save to cookies
    - Rename isAuthPage to isAuthenticationPage for consistency
    - Remove unused  secFetchMode, accept, and isNavigationRequest variables

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,44 +1,70 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { getNewAccessToken } from './lib/http/get-access-token';
+import { SessionExpiredError } from './errors/session-expired-error';
 import {
   isTokenExpired,
   ACCESS_COOKIE_NAME,
   REFRESH_COOKIE_NAME,
+  COOKIE_OPTIONS,
+  closeSession,
 } from './helpers/auth-token-manager';
-import { refreshAccessTokenAction } from './app/actions/auth/refresh-access-token';
+import { jwtDecode } from 'jwt-decode';
+
+function isTokenValid(token?: string) {
+  if (!token) return false;
+  try {
+    jwtDecode(token);
+    return isTokenExpired(token);
+  } catch {
+    return false;
+  }
+}
 
 export async function proxy(request: NextRequest) {
   const url = request.nextUrl.clone();
   const path = request.nextUrl.pathname;
-  const isAuthPage = path === '/login' || path === '/signup';
-  const isAuthenticated = request.cookies.get(REFRESH_COOKIE_NAME)?.value;
+
+  const isAuthenticationPage = path === '/login' || path === '/signup';
+  const refreshToken = request.cookies.get(REFRESH_COOKIE_NAME)?.value;
   const accessToken = request.cookies.get(ACCESS_COOKIE_NAME)?.value;
+  const isAuthenticated = isTokenValid(refreshToken);
+
+  const isPageRequest =
+    request.method === 'GET' &&
+    request.headers.get('accept')?.includes('text/html');
 
   if (path.startsWith('/_next') || path.startsWith('/_static')) {
     return NextResponse.next();
   }
 
-  const secFetchMode = request.headers.get('sec-fetch-mode') ?? '';
-  const accept = request.headers.get('accept') ?? '';
-  const isNavigationRequest =
-    request.method === 'GET' &&
-    (secFetchMode === 'navigate' || accept.includes('text/html'));
-
-  if (isAuthenticated && isAuthPage && isNavigationRequest) {
+  if (isAuthenticated && isAuthenticationPage && isPageRequest) {
     url.pathname = '/desk';
     return NextResponse.redirect(url);
   }
 
-  if (!isAuthenticated && !isAuthPage && isNavigationRequest) {
+  if (!isAuthenticated && !isAuthenticationPage && isPageRequest) {
     url.pathname = '/login';
+    url.searchParams.set('session-expired', 'true');
     return NextResponse.redirect(url);
   }
 
-  if (!accessToken || isTokenExpired(accessToken)) {
+  if (isAuthenticated && (!!accessToken || !isTokenValid(accessToken))) {
     try {
-      await refreshAccessTokenAction();
+      const newAccess = await getNewAccessToken(refreshToken!);
+      const response = NextResponse.next();
+      response.cookies.set(
+        ACCESS_COOKIE_NAME,
+        newAccess.accessToken,
+        COOKIE_OPTIONS,
+      );
+
+      return response;
     } catch (error) {
-      if (process.env.NODE_ENV === 'development') {
-        console.log('PROXY ERROR: ', error);
+      if (error instanceof SessionExpiredError) {
+        await closeSession();
+        url.pathname = '/login';
+        url.searchParams.set('session-expired', 'true');
+        return NextResponse.redirect(url);
       }
     }
   }
