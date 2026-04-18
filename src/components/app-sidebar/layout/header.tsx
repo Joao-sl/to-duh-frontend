c3a5e70e18f11dea867bd928129d5278f17d3f91
João <joaosl.dev@gmail.com>
2026-04-18 12:49:12 -0300

    feat: add ThemeToggleButton to sidebar header

--- src/components/app-sidebar/layout/header.tsx
+++ src/components/app-sidebar/layout/header.tsx
@@ -12,10 +12,12 @@ import {
   SidebarTrigger,
   useSidebar,
 } from '@/components/ui/sidebar';
+import { ThemeToggleButton } from '@/components/ui/theme-toggle-button';
+import { Button } from '@/components/ui/button';
 
 function AppSidebarHeader() {
   const { user } = useUserContext();
-  const { open } = useSidebar();
+  const { open, isMobile } = useSidebar();
 
   return (
     <SidebarHeader className='py-2'>
@@ -24,15 +26,32 @@ function AppSidebarHeader() {
           className={cn(
             'absolute text-xs text-primary left-2 top-1/2 -translate-y-1/2 transition duration-300 ease-in-out whitespace-nowrap',
             {
-              'opacity-0 -translate-y-4 pointer-events-none': !open,
-              'opacity-100 translate-x-0': open,
+              'opacity-0 -translate-y-4 pointer-events-none':
+                !open && !isMobile,
+              'opacity-100 translate-x-0': open && !isMobile,
             },
           )}
         >
           TO~DUH
         </h1>
 
-        <SidebarTrigger className='ml-auto' />
+        <Button
+          variant='ghost'
+          size='icon'
+          asChild
+          aria-label='Toggle theme'
+          className={cn('size-8 ml-auto text-muted-foreground', {
+            hidden: !open && !isMobile,
+          })}
+        >
+          <ThemeToggleButton />
+        </Button>
+
+        <SidebarTrigger
+          className={cn('text-muted-foreground', {
+            'ml-auto': !open && !isMobile,
+          })}
+        />
       </div>
 
       <SidebarMenu>
