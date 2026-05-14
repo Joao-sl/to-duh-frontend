9d2cbae944d71c292929039fc6ece18f9b944f83
João <joaosl.dev@gmail.com>
2026-05-14 12:01:08 -0300

    feat: add update section form with configurable submission mode

---
+++ src/components/section-form/update-section-form.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import { Button } from '../ui/button';
+import { Spinner } from '../ui/spinner';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SectionData } from '@/lib/http/types/section';
+import { FormProvider, useForm } from 'react-hook-form';
+import { SectionFormFields } from './section-form-fields';
+import { handleApiErrors } from '@/helpers/handle-api-errors';
+import { updateProjectAction } from '@/app/actions/project/update-project';
+import {
+  type UpdateSectionSchema,
+  updateSectionSchema,
+} from '@/validations/schemas/sections';
+
+type UpdateProjectFormProps = {
+  sectionId: number;
+  initialValues: SectionData;
+  onSuccess?: (data: SectionData) => void;
+  onCancel?: (formIsDirty: boolean) => void;
+  submissionMode?: 'server-action' | 'route-handler';
+};
+
+function UpdateSectionForm({
+  sectionId,
+  initialValues,
+  onSuccess,
+  onCancel,
+  submissionMode = 'server-action',
+}: UpdateProjectFormProps) {
+  const form = useForm<UpdateSectionSchema>({
+    resolver: zodResolver(updateSectionSchema),
+    defaultValues: {
+      name: initialValues.name,
+    },
+    mode: 'onSubmit',
+  });
+
+  const { formState, reset, setError, getValues, handleSubmit } = form;
+
+  async function onSubmit() {
+    let response;
+    const data = getValues(undefined, { dirtyFields: true });
+
+    if (Object.keys(data).length === 0) {
+      return;
+    }
+
+    if (submissionMode === 'server-action') {
+      response = await updateProjectAction(sectionId, data);
+    } else {
+      const res = await fetch(`/api/projects/${sectionId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+
+      response = await res.json();
+    }
+
+    if (!response.success) {
+      handleApiErrors(response, setError, updateSectionSchema);
+      return;
+    }
+
+    if (onSuccess) {
+      const { success, ...rest } = response;
+      void success;
+      onSuccess(rest);
+    }
+  }
+
+  function handleOnCancel() {
+    if (onCancel) {
+      return onCancel(formState.isDirty);
+    }
+
+    return reset();
+  }
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <SectionFormFields hideLabels />
+
+        <div className='flex justify-end gap-2 mt-2'>
+          <Button
+            type='button'
+            size='sm'
+            variant='secondary'
+            disabled={formState.isSubmitting}
+            aria-disabled={formState.isSubmitting}
+            onClick={handleOnCancel}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            size='sm'
+            type='submit'
+            disabled={formState.isSubmitting || !formState.isDirty}
+            aria-disabled={formState.isSubmitting}
+            className='w-17.25'
+          >
+            {formState.isSubmitting ? <Spinner /> : 'Update'}
+          </Button>
+        </div>
+      </form>
+    </FormProvider>
+  );
+}
+
+export { UpdateSectionForm };
