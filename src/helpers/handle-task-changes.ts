aa8eb745a0441e44227a99218295ecdc8092c63b
João <joaosl.dev@gmail.com>
2026-05-28 11:36:17 -0300

    feat: add helpers to update and remove tasks from project board

---
+++ src/helpers/handle-task-changes.ts
@@ -0,0 +1,60 @@
+import { TaskData } from '@/lib/http/types/task';
+import { ProjectBoardData } from '@/lib/http/types/project';
+
+export function updateTaskInBoard(
+  state: ProjectBoardData,
+  taskData: TaskData,
+): ProjectBoardData {
+  if (taskData.section_id == null) {
+    return {
+      ...state,
+      tasks_without_sections: state.tasks_without_sections.map(task =>
+        task.id === taskData.id ? taskData : task,
+      ),
+    };
+  }
+
+  return {
+    ...state,
+    sections: state.sections.map(section => {
+      if (section.id !== taskData.section_id) {
+        return section;
+      }
+
+      return {
+        ...section,
+        tasks: section.tasks.map(task =>
+          task.id === taskData.id ? taskData : task,
+        ),
+      };
+    }),
+  };
+}
+
+export function removeTaskFromBoard(
+  state: ProjectBoardData,
+  taskData: Pick<TaskData, 'id' | 'section_id'>,
+): ProjectBoardData {
+  if (taskData.section_id == null) {
+    return {
+      ...state,
+      tasks_without_sections: state.tasks_without_sections.filter(
+        task => task.id !== taskData.id,
+      ),
+    };
+  }
+
+  return {
+    ...state,
+    sections: state.sections.map(section => {
+      if (section.id !== taskData.section_id) {
+        return section;
+      }
+
+      return {
+        ...section,
+        tasks: section.tasks.filter(task => task.id !== taskData.id),
+      };
+    }),
+  };
+}
