fe13f69b10968e8211e9a67ea25a92a2364ca721
João <joaosl.dev@gmail.com>
2026-06-29 19:55:37 -0300

    feat: add helper to add section in board
    Also add return types to functions

--- src/helpers/handle-section-changes.ts
+++ src/helpers/handle-section-changes.ts
@@ -1,10 +1,20 @@
-import { ProjectBoardData } from '@/lib/http/types/project';
-import { SectionData } from '@/lib/http/types/section';
+import { type ProjectBoardData } from '@/lib/http/types/project';
+import { type SectionData } from '@/lib/http/types/section';
+
+export function addSectionInBoard(
+  state: ProjectBoardData,
+  data: SectionData,
+): ProjectBoardData {
+  return {
+    ...state,
+    sections: [...state.sections, { tasks: [], ...data }],
+  };
+}
 
 export function updateSectionInBoard(
   state: ProjectBoardData,
   data: SectionData,
-) {
+): ProjectBoardData {
   return {
     ...state,
     sections: state.sections.map(state =>
@@ -16,7 +26,7 @@ export function updateSectionInBoard(
 export function removeSectionFormBoard(
   state: ProjectBoardData,
   data: Pick<SectionData, 'id'>,
-) {
+): ProjectBoardData {
   return {
     ...state,
     sections: state.sections.filter(section => section.id !== data.id),
