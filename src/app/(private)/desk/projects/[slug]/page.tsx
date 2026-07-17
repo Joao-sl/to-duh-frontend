85df4878717bb3069e76b8b367fd1b522a937052
João <joaosl.dev@gmail.com>
2026-07-17 19:11:42 -0300

    feat: implement project details page with board fetching, loading, and error handling

--- src/app/(private)/desk/projects/[slug]/page.tsx
+++ src/app/(private)/desk/projects/[slug]/page.tsx
@@ -1,3 +1,39 @@
-export default async function ProjectDetails() {
-  return 'Project Details';
+import Loading from './loading';
+import { Suspense } from 'react';
+import { Container } from '@/components/ui/container';
+import { getProjectBoard } from '@/lib/http/get-project-board';
+import { ProjectDetailsLayout } from '@/components/project-details';
+import { ProjectDetailsBreadcrumb } from '@/components/project-details/navigation/breadcrumb';
+import { notFound } from 'next/navigation';
+
+type ProjectDetailsProps = {
+  params: Promise<{ slug: string }>;
+};
+
+export default async function ProjectDetails({ params }: ProjectDetailsProps) {
+  const { slug } = await params;
+  const projectId = Number(slug.split('-').at(-1));
+
+  if (Number.isNaN(projectId)) {
+    notFound();
+  }
+
+  const projectBoard = await getProjectBoard(projectId);
+
+  if (projectBoard.status === 404) {
+    notFound();
+  }
+
+  if (!projectBoard.success) {
+    throw new Error('Board fetch error');
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <ProjectDetailsBreadcrumb />
+      <Container>
+        <ProjectDetailsLayout project={projectBoard.data} />
+      </Container>
+    </Suspense>
+  );
 }
