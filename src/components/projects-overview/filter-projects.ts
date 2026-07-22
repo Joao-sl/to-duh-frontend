ce902b6e1666969fe201fe59966cfe0b09b56ce1
João <joaosl.dev@gmail.com>
2026-07-22 17:38:11 -0300

    fix: remove unnecessary function wrapper from filterProjects

--- src/components/projects-overview/filter-projects.ts
+++ src/components/projects-overview/filter-projects.ts
@@ -1,23 +1,18 @@
 import type { ProjectData } from '@/lib/http/types/project';
-import type { ProjectFilterFields } from './filter-button';
+import type { FilterField } from './filter-button';
 
-function filterProjects(
-  field: ProjectFilterFields,
-  initialData: ProjectData[],
-) {
-  return () => {
-    switch (field) {
-      case 'all':
-        return initialData;
+function filterProjects(field: FilterField, initialData: ProjectData[]) {
+  switch (field) {
+    case 'all':
+      return initialData;
 
-      case 'favorites':
-        const filtered = initialData.filter(proj => proj.is_favorite === true);
-        return filtered;
+    case 'favorites':
+      const filtered = initialData.filter(proj => proj.is_favorite === true);
+      return filtered;
 
-      default:
-        return initialData;
-    }
-  };
+    default:
+      return initialData;
+  }
 }
 
 export { filterProjects };
