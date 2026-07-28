cf220b39d966f00ac3750d8aedb72da459565afd
João <joaosl.dev@gmail.com>
2026-07-28 11:17:49 -0300

    fix: allow description to be null

--- src/lib/http/types/project.ts
+++ src/lib/http/types/project.ts
@@ -1,11 +1,11 @@
-import { SectionWithTask } from './section';
-import { TaskData } from './task';
+import type { SectionWithTask } from './section';
+import type { TaskData } from './task';
 
 export type ProjectData = {
   id: number;
   user_id: number;
   name: string;
-  description: string;
+  description: string | null;
   is_favorite: boolean;
   is_archived: boolean;
   created_at: string;
