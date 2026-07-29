daa2fd126016f031a1eb30e4b1b415024ad995a2
João <joaosl.dev@gmail.com>
2026-07-29 11:50:02 -0300

    feat: add ProjectsOverview with suspense to Projects page

--- src/app/(private)/desk/projects/page.tsx
+++ src/app/(private)/desk/projects/page.tsx
@@ -1,3 +1,23 @@
-export default function Projects() {
-  return 'PROJECTS PAGE';
+import Loading from './loading';
+import { Suspense } from 'react';
+import type { Metadata } from 'next';
+import { getProjects } from '@/lib/http/get-projects';
+import { ProjectsOverview } from '@/components/projects-overview';
+
+export const metadata: Metadata = {
+  title: 'Projects',
+};
+
+export default async function Projects() {
+  const projects = await getProjects();
+
+  if (!projects.success) {
+    throw new Error('Projects fetch error');
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <ProjectsOverview initialData={projects.data} />
+    </Suspense>
+  );
 }
