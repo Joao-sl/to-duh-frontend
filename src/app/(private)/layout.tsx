4899fbdc885eb994e53bfcb5f2b0b9df2c824af1
João <joaosl.dev@gmail.com>
2026-08-04 17:06:51 -0300

    refactor: switch to inset variant with SidebarInset wrapper

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,6 +1,7 @@
+import { cn } from '@/utils/cn';
 import { Suspense } from 'react';
 import { AppSidebar } from '@/components/app-sidebar';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebarSkeleton } from '@/components/ui/skeletons/app-sidebar-skeleton';
 import { MobileTrigger } from '@/components/app-sidebar/layout/mobile-trigger';
 import { SidebarAutoClose } from '@/components/app-sidebar/utils/auto-close';
@@ -18,10 +19,22 @@ export default function AppLayout({ children, account }: AppLayoutProps) {
         <AppSidebar account={account} />
       </Suspense>
 
-      <main id='main-content' className='flex-1'>
-        <MobileTrigger />
-        {children}
-      </main>
+      <SidebarInset
+        className={cn(
+          'h-svh md:h-[calc(100svh-1rem)] md:peer-data-[variant=inset]:rounded-2xl sm:px-2',
+        )}
+      >
+        <div
+          id='main-content'
+          className={cn(
+            'flex-1 min-h-0 overflow-auto',
+            'scrollbar-thin scrollbar-gutter-stable scrollbar-thumb-primary',
+          )}
+        >
+          <MobileTrigger />
+          {children}
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
