f2ea8ce6e067bd21c471dd3782e5ca90927baab1
João <joaosl.dev@gmail.com>
2026-08-10 10:55:57 -0300

    feat: add tasks fetching and TasksOverview with Suspense

--- src/app/(private)/desk/tasks/page.tsx
+++ src/app/(private)/desk/tasks/page.tsx
@@ -1,3 +1,23 @@
-export default function Tasks() {
-  return 'TASKS PAGE';
+import Loading from './loading';
+import { Suspense } from 'react';
+import { type Metadata } from 'next';
+import { getTasksList } from '@/lib/http/task/get';
+import { TasksOverview } from '@/components/tasks-overview';
+
+export const metadata: Metadata = {
+  title: 'Tasks',
+};
+
+export default async function Tasks() {
+  const tasks = await getTasksList();
+
+  if (!tasks.success) {
+    throw new Error('Tasks fetch error');
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <TasksOverview initialData={tasks.data} />
+    </Suspense>
+  );
 }
