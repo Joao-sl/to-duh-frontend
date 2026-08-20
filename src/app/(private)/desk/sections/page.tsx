16177acde33997672808be720fa3c25917099c72
João <joaosl.dev@gmail.com>
2026-08-20 12:08:48 -0300

    feat: add sections page with fetching and Suspense

--- src/app/(private)/desk/sections/page.tsx
+++ src/app/(private)/desk/sections/page.tsx
@@ -1,3 +1,23 @@
-export default function Sections() {
-  return 'SECTIONS PAGE';
+import { Metadata } from 'next';
+import Loading from './loading';
+import { Suspense } from 'react';
+import { getSectionsList } from '@/lib/http/section/get';
+import { SectionsOverview } from '@/components/sections-overview';
+
+export const metadata: Metadata = {
+  title: 'Sections',
+};
+
+export default async function Sections() {
+  const sections = await getSectionsList();
+
+  if (!sections.success) {
+    throw new Error('Sections fetch error');
+  }
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <SectionsOverview initialData={sections.data} />
+    </Suspense>
+  );
 }
